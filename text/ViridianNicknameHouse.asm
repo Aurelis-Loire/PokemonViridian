bf6d9e170df3981c0723d215145d2406648dfd24
PaladinJohn <Tigerquan@aol.com>
2023-03-28 02:59:34 -0400

    Edited Nickname House Text
    Turned Speary the Spearow into Sanders the Pidgey since Spearow is no longer in the mod.

--- text/ViridianNicknameHouse.asm
+++ text/ViridianNicknameHouse.asm
@@ -14,10 +14,10 @@ _ViridianHouseText2::
 	done
 
 _ViridianHouseText_1d5b1::
-	text "SPEARY: Tetweet!"
+	text "SANDERS: Tetweet!"
 	done
 
 _ViridianHouseText4::
-	text "SPEAROW"
-	line "Name: SPEARY"
+	text "PIDGEY"
+	line "Name: SANDERS"
 	done
