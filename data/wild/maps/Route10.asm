d0b9725ccbab87a3f7dff1e51788b3a0d3b28fa2
PaladinJohn <Tigerquan@aol.com>
2023-09-13 03:19:46 -0400

    Updated Route 10 Encounters
    Added Ducklet and Mareep and removed placeholders from Route 10.

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -1,26 +1,15 @@
 Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 16, VOLTORB
-	db 16, SCATTERBUG
+	db 16, DUCKLETT
 	db 14, VOLTORB
-IF DEF(_RED)
 	db 11, EKANS
-	db 13, SCATTERBUG
+	db 13, DUCKLETT
 	db 15, EKANS
 	db 17, VOLTORB
-	db 17, SCATTERBUG
-	db 13, EKANS
+	db 17, DUCKLETT
 	db 17, EKANS
-ENDC
-IF DEF(_BLUE)
-	db 11, CLEFABLE
-	db 13, SCATTERBUG
-	db 15, CLEFABLE
-	db 17, VOLTORB
-	db 17, SCATTERBUG
-	db 13, CLEFABLE
-	db 17, CLEFABLE
-ENDC
+	db 13, MAREEP
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
