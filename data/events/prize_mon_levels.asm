ca57fc3fb8960aeb42f1158949a37793af10b383
PaladinJohn <Tigerquan@aol.com>
2023-09-21 03:53:17 -0400

    Updated Game Corner Prizes
    Added Pikachu and Eevee to the Game Corner prizes due to no Nidorans or Porygon.

--- data/events/prize_mon_levels.asm
+++ data/events/prize_mon_levels.asm
@@ -1,19 +1,8 @@
 PrizeMonLevelDictionary:
-IF DEF(_RED)
 	db ABRA,      9
 	db CLEFAIRY,  8
-	db PRIMEAPE, 17
+	db PIKACHU,   12
 
 	db DRATINI,  18
 	db SCYTHER,  25
-	db PORYGON,  26
-ENDC
-IF DEF(_BLUE)
-	db ABRA,      6
-	db CLEFAIRY, 12
-	db NIDORINO, 17
-
-	db IVYSAUR,  20
-	db DRATINI,  24
-	db PORYGON,  18
-ENDC
+	db EEVEE,    15
