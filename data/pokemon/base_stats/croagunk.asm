8df1ef1cd9b8b8c5671d89c57bcce3bbcf33b68b
PaladinJohn <Tigerquan@aol.com>
2023-09-30 04:30:07 -0400

    Minor Croagunk Adjustments
    Replaced Dig with the less powerful Bone Club since Dig was too good.
    
    Also gave a Super Nerd an additional Croagunk.

--- data/pokemon/base_stats/croagunk.asm
+++ data/pokemon/base_stats/croagunk.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/croagunk.pic", 0, 1 ; sprite dimensions
 	dw CroagunkPicFront, CroagunkPicBack
 
-	db ASTONISH, DIG, NO_MOVE, NO_MOVE ; level 1 learnset
+	db ASTONISH, BONE_CLUB, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
