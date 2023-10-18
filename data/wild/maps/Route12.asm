a7bc840ae123a0ab0dea488d438a9e187021ecd8
PaladinJohn <Tigerquan@aol.com>
2023-10-18 03:43:34 -0400

    Updated Route 12
    Changed Trainer parties for Route 12 as well as wild encounters.

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,29 +1,15 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
-IF DEF(_RED)
-	db 24, CLOYSTER
-	db 25, PIDGEY
-	db 23, PIDGEY
+	db 24, STEENEE
+	db 25, PIDGEOTTO
+	db 23, PIDGEOTTO
 	db 24, VENONAT
-	db 22, CLOYSTER
+	db 22, STEENEE
 	db 26, VENONAT
-	db 26, CLOYSTER
-	db 27, PIDGEY
-	db 28, GENGAR
-	db 30, GENGAR
-ENDC
-IF DEF(_BLUE)
-	db 24, BELLSPROUT
-	db 25, PIDGEY
-	db 23, PIDGEY
-	db 24, VENONAT
-	db 22, BELLSPROUT
-	db 26, VENONAT
-	db 26, BELLSPROUT
-	db 27, PIDGEY
-	db 28, WEEPINBELL
-	db 30, WEEPINBELL
-ENDC
+	db 26, STEENEE
+	db 27, PIDGEOTTO
+	db 28, STEENEE
+	db 30, TSAREENA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
