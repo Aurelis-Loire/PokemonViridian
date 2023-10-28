a47babdb06a402c5a60acb91ad6b8c8899911b24
PaladinJohn <Tigerquan@aol.com>
2023-10-28 02:20:24 -0400

    Updated Safari Zone Encounters
    Safari Zone encounters are updated across all zones.

--- data/wild/maps/SafariZoneEast.asm
+++ data/wild/maps/SafariZoneEast.asm
@@ -1,29 +1,15 @@
 SafariZoneEastWildMons:
 	def_grass_wildmons 30 ; encounter rate
-IF DEF(_RED)
-	db 24, BULBASAUR
+	db 24, NIDORINO
 	db 26, DODUO
-	db 22, PARAS
+	db 22, CUBONE
 	db 25, EXEGGCUTE
-	db 33, ALAKAZAM
+	db 33, NIDORINO
 	db 23, EXEGGCUTE
-	db 24, CHARMANDER
-	db 25, PIDGEOT
+	db 24, LICKITUNG
+	db 25, LOPUNNY
 	db 25, KANGASKHAN
 	db 28, SCYTHER
-ENDC
-IF DEF(_BLUE)
-	db 24, CHARMANDER
-	db 26, DODUO
-	db 22, PARAS
-	db 25, EXEGGCUTE
-	db 33, PRIMEAPE
-	db 23, EXEGGCUTE
-	db 24, BULBASAUR
-	db 25, PIDGEOT
-	db 25, KANGASKHAN
-	db 28, CHARMELEON
-ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/SafariZoneWest.asm
+++ data/wild/maps/SafariZoneWest.asm
@@ -1,23 +1,12 @@
 SafariZoneWestWildMons:
 	def_grass_wildmons 30 ; encounter rate
-IF DEF(_RED)
-	db 25, BULBASAUR
+	db 25, NIDORINO
 	db 26, DODUO
 	db 23, VENONAT
 	db 24, EXEGGCUTE
-	db 33, ALAKAZAM
+	db 33, NIDORINO
 	db 26, EXEGGCUTE
-	db 25, CHARMANDER
-ENDC
-IF DEF(_BLUE)
-	db 25, CHARMANDER
-	db 26, DODUO
-	db 23, VENONAT
-	db 24, EXEGGCUTE
-	db 33, PRIMEAPE
-	db 26, EXEGGCUTE
-	db 25, BULBASAUR
-ENDC
+	db 25, LICKITUNG
 	db 31, VENOMOTH
 	db 26, TAUROS
 	db 28, KANGASKHAN
