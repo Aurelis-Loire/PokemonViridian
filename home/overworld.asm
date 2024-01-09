d1d65bf3565d26af69e9b529f96998fecf9d1cf5
PaladinJohn <Tigerquan@aol.com>
2024-01-09 03:40:18 -0500

    Trainer Rematches 2
    Finished Implementing Trainer Rematches. May need more testing to ensure there's no weirdness.
    
    Should be able to rematch schmuck trainers from now on.

--- home/overworld.asm
+++ home/overworld.asm
@@ -2468,3 +2468,18 @@ LoadDestinationWarpPosition::
 	ldh [hLoadedROMBank], a
 	ld [MBC1RomBank], a
 	ret
+
+TrainerRematch:
+	xor a
+	CheckEvent EVENT_REMATCH_DELAY
+	jr nz, .skip_rematch_choice
+	ld hl, RematchTrainerText
+	call PrintText
+	call NoYesChoice
+	ld a, [wCurrentMenuItem]
+	and a
+	ret nz
+.skip_rematch_choice
+	ResetEvent EVENT_REMATCH_DELAY
+	xor a
+	ret
