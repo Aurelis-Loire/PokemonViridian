f08253171f1a46a6b58b1acaeb31cc2b4fd817b3
PaladinJohn <Tigerquan@aol.com>
2024-02-03 03:10:22 -0500

    Back Sprites 13
    More Celadon Sprint Back Sprites.
    
    Also changed around some in game trades to make more sense.

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -6,14 +6,14 @@ TradeMons:
 	; from the Japanese Blue trades, which used species that evolve.
 	; Japanese Red and Green used TRADE_DIALOGSET_CASUAL, and had
 	; the same species as English Red and Blue.
-	db QUAGSIRE,   ZUBAT,     TRADE_DIALOGSET_CASUAL,    "ZUBATMAN@@@"
-	db ABRA,       WAILMER,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
+	db QUAGSIRE,   MAGNEMITE, TRADE_DIALOGSET_CASUAL,    "BOLTS@@@@@@"
+	db BEEDRILL,   EXEGGCUTE, TRADE_DIALOGSET_CASUAL,    "WIGGLES@@@@"
 	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
 	db PONYTA,     SQUIRTLE,  TRADE_DIALOGSET_CASUAL,    "SAILOR@@@@@"
-	db RALTS,      BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "DUX@@@@@@@@"
+	db RALTS,      CUBONE,    TRADE_DIALOGSET_HAPPY,     "PEANUT@@@@@"
 	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC@@@@@@@"
 	db ZUBAT,      VOLTORB,   TRADE_DIALOGSET_EVOLUTION, "ZAPPY@@@@@@"
 	db RAICHU,     ELECTRODE, TRADE_DIALOGSET_CASUAL,    "DORIS@@@@@@"
 	db VENONAT,    KIRLIA,    TRADE_DIALOGSET_HAPPY,     "CRINKLES@@@"
-	db ELECTRODE,  EEVEE,     TRADE_DIALOGSET_HAPPY,     "FRANK@@@@@@"
+	db VULPIX,     GIBLE,     TRADE_DIALOGSET_HAPPY,     "CHOMPERS@@@"
 	assert_table_length NUM_NPC_TRADES
