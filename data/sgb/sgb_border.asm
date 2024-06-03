9182ed64b589bac1c63cc7afa258ab72ba3bf834
PaladinJohn <Tigerquan@aol.com>
2024-06-03 01:43:40 -0400

    Viridian Theme
    Changed the title to indicate it's the Viridian version and changed the SGB background to the Pokemon Green background.

--- data/sgb/sgb_border.asm
+++ data/sgb/sgb_border.asm
@@ -1,6 +1,7 @@
 BorderPalettes:
 IF DEF(_RED)
-	INCBIN "gfx/sgb/red_border.tilemap"
+	;INCBIN "gfx/sgb/red_border.tilemap"
+	INCBIN "gfx/sgb/green_border.tilemap"
 ENDC
 IF DEF(_BLUE)
 	INCBIN "gfx/sgb/blue_border.tilemap"
@@ -10,9 +11,12 @@ ENDC
 
 IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB1
-	RGB 25,22,25
-	RGB 25,17,21
-	RGB 24,14,12
+	;RGB 25,22,25
+	;RGB 25,17,21
+	;RGB 24,14,12
+	RGB 25,31,18
+	RGB 18,25,12
+	RGB 12,20,9
 ENDC
 IF DEF(_BLUE)
 	RGB 0,0,0 ; PAL_SGB1 (the first color is not defined, but if used, turns up as 30,29,29... o_O)
@@ -25,8 +29,10 @@ ENDC
 
 IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB2
-	RGB 22,31,16
-	RGB 27,20,6
+	;RGB 22,31,16
+	;RGB 27,20,6
+	RGB 15,18,27
+	RGB 24,19,7
 	RGB 15,15,15
 ENDC
 IF DEF(_BLUE)
@@ -40,9 +46,12 @@ ENDC
 
 IF DEF(_RED)
 	RGB 30,29,29 ; PAL_SGB3
-	RGB 31,31,17
-	RGB 18,21,29
-	RGB 15,15,15
+	;RGB 31,31,17
+	;RGB 18,21,29
+	;RGB 15,15,15
+	RGB 30,27,4
+	RGB 29,18,20
+	RGB 13,15,16
 ENDC
 IF DEF(_BLUE)
 	RGB 30,29,29 ; PAL_SGB3
@@ -55,7 +64,8 @@ ENDC
 
 SGBBorderGraphics:
 IF DEF(_RED)
-	INCBIN "gfx/sgb/red_border.2bpp"
+	;INCBIN "gfx/sgb/red_border.2bpp"
+	INCBIN "gfx/sgb/green_border.2bpp"
 ENDC
 IF DEF(_BLUE)
 	INCBIN "gfx/sgb/blue_border.2bpp"
--- engine/movie/title.asm
+++ engine/movie/title.asm
@@ -121,7 +121,7 @@ DisplayTitleScreen:
 	call EnableLCD
 
 IF DEF(_RED)
-	ld a, STARTER1 ; which Pokemon to show first on the title screen
+	ld a, STARTER3 ; which Pokemon to show first on the title screen
 ENDC
 IF DEF(_BLUE)
 	ld a, STARTER2 ; which Pokemon to show first on the title screen
@@ -394,14 +394,15 @@ INCLUDE "data/pokemon/title_mons.asm"
 
 ; prints version text (red, blue)
 PrintGameVersionOnTitleScreen:
-	hlcoord 7, 8
+	hlcoord 5, 8
 	ld de, VersionOnTitleScreenText
 	jp PlaceString
 
 ; these point to special tiles specifically loaded for that purpose and are not usual text
 VersionOnTitleScreenText:
 IF DEF(_RED)
-	db $60,$61,$7F,$65,$66,$67,$68,$69,"@" ; "Red Version"
+	;db $60,$61,$7F,$65,$66,$67,$68,$69,"@" ; "Red Version"
+	db $60,$61,$62,$63,$64,$65,$66,$67,$68,$69,"@" ; "Viridian Version"
 ENDC
 IF DEF(_BLUE)
 	db $61,$62,$63,$64,$65,$66,$67,$68,"@" ; "Blue Version"
